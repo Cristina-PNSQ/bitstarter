3fa4e9276b7169c9148e6edd3811686a84d9fdae
Cristina-PNSQ <cristina.pienescu@gmail.com>
2013-07-17 02:25:28 +0000

    Changed the web.js to read from index.html

--- web.js
+++ web.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var getString = function(){
+    var buff = fs.readFileSync("index.html");
+    return buff.toString();
+
+ };
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(getString());
 });
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
